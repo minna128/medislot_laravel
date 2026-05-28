fdbf4fbdac4c840654f947e0335cd5497c300e80
minna128 <minnahmuz@gmail.com>
2026-05-29 05:17:17 +0530

    Fix favicon - remove empty ico file

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <link rel="icon" type="image/svg+xml" href="{{ asset('logo.svg') }}">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="{{ asset('logo.svg') }}">        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'MediSlot') }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
